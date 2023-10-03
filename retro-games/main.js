0309d74c015817445c5abd67104e4be0efd3feef
Anthony Obiako <anthonyobiakoc@gmail.com>
2023-10-03 15:55:29 +0100

    Slider added to carousel

--- retro-games/main.js
+++ retro-games/main.js
@@ -9,3 +9,29 @@ navToggle.addEventListener("click", () => {
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
 });
+
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: true,
+  arrows: false,
+  dots: true,
+});
+
+// const slider = new A11YSlider(document.querySelector(".slider"), {
+//   slidesToShow: 1,
+//   arrows: true, // arrows enabled 767px and down
+//   dots: false,
+//   responsive: {
+//     768: {
+//       slidesToShow: 2,
+//       arrows: false,
+//     },
+//     960: {
+//       disable: true, // slider disabled 960px to 1279px
+//     },
+//     1280: {
+//       disable: false,
+//       slidesToShow: 4,
+//       dots: true, // dots enabled 1280px and up
+//     },
+//   },
+// });
